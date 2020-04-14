f32c352bada751c4545c4cfd42b659b4c9016efc
Galaxine <oezdemir50@gmail.com>
2020-04-14 20:49:44 +0200

    Build the first barebone parts of the current Usertory. the idea is to create a partly JavaFX application that is structured akin to Model-View-Controller.

--- src/main/java/GUIController.java
+++ src/main/java/GUIController.java
@@ -1,2 +1,18 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+/**
+ * This is our Controller (Observer) takes Userinput and works it out. Typically
+ * it gets the Eventlistener, which listens to for an example button-clicks, and
+ * triggers actions which the Model let loose
+ */
 public class GUIController {
+    @FXML
+    TextArea textArea;
+
+    public void initialize() {
+        Label l = new Label("I am a new Label");
+
+    }
 }
---
+++ src/main/java/model/Model.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * This is our Model, which encompasses the
+ * logic behind the GUI of the LocalPlayer.
+ * It is independent of the View and Controller.
+ * It provides methods for the data requests and
+ * informs the observer (controller) if any changes
+ * occur.
+ * Addendum: this class shouldn't have any javaFX relevant
+ * elements. Any.
+ */
+public class Model {
+}
