729aea549b719d31472ad10e55ab116219520637
Galaxine <oezdemir50@gmail.com>
2020-04-20 20:24:26 +0200

    I have tried to fix the setNextTrack() not firing at Model and the PropertyChange not registering it. Did I really understand how Listeners work and how I can use them for the MVC design pattern?

--- src/main/java/GUIController.java
+++ src/main/java/GUIController.java
@@ -14,13 +14,28 @@ public class GUIController implements PropertyChangeListener {
     @FXML
     TextArea textArea;
 
-    public void initialize() {
+  /*  public void initialize() {
         Label l = new Label("I am a new Label");
 
-    }
+    }*/
 
+    /**
+     * Listens to the model class and at first should change the View (fxml) to show the current
+     * @param evt for now, it should register the property track changing at {@link model.Model}'s setNexTrack
+     *            TODO: Fix this so the controller recognizes the change and overwrite's the old track's metadata
+     *              with the new one's .
+     */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+        System.out.println("musicListener");
+        if (evt.getPropertyName().equals("track")) {
+            System.out.println("listener active");
+            TextArea textArea = new TextArea(evt.getNewValue().toString());
+            setTextArea(textArea);
+        }
+    }
 
+    public void setTextArea(TextArea textArea) {
+        this.textArea = textArea;
     }
 }
--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -4,6 +4,7 @@ import FileReader.MP3File;
 import FileReader.Playlist;
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
@@ -17,13 +18,19 @@ import java.beans.PropertyChangeSupport;
  * occur.
  * Addendum: this class shouldn't have any javaFX relevant
  * elements. Any.
+ * TODO: I seem to repeat myself playing the tracks. that needs to be fixed. New exclusive class?
  */
 public class Model {
     private MediaPlayerFactory factory = new MediaPlayerFactory();
     private MediaPlayer mediaPlayer = factory.mediaPlayers().newMediaPlayer();
     private Playlist playlist;
     private PropertyChangeSupport support;
+    private MP3File track = null;
 
+    /**
+     *
+     * @param filedirectory self explanatory, assigned to the playlist class constructor
+     */
     public Model(String filedirectory) {
         support = new PropertyChangeSupport(this);
         playlist = new Playlist(filedirectory);
@@ -31,29 +38,74 @@ public class Model {
         playNextTrack();
     }
 
+    /**
+     * Should play the next track AFTER the track before has finished.
+     */
     private void playNextTrack() {
+        mediaPlayer.events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+            @Override
+            public void finished(MediaPlayer mediaPlayer) {
+                mediaPlayer.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        mediaPlayer.media().play(playlist.getPlaylist().getFirst().getAbsoluteFilename());
+                    }
+                });
+            }
+        });
     }
 
+    /**
+     * plays the first track.
+     */
     private void playTrack() {
+        mediaPlayer.submit(new Runnable() {
+            @Override
+            public void run() {
+                assignFirstPieceToTrack();
+                mediaPlayer.media().play(playlist.getPlaylist().getFirst().getAbsoluteFilename());
+            }
+        });
+    }
+
+    /**
+     *this method sets the next track and then fires the property change.
+     */
+    public void setNextTrack() {
+        if(!this.track.equals(null)) {
+            MP3File oldTrack = this.track;
+            MP3File newTrack = playlist.getPlaylist().getFirst();
+            this.track = newTrack;
+            support.firePropertyChange("track", oldTrack, newTrack);
+        } else {
+            MP3File oldTrack = this.track;
+            playlist.nextTrack();
+            MP3File newTrack = playlist.getPlaylist().getFirst();
+            this.track = newTrack;
+            support.firePropertyChange("track", oldTrack, newTrack);
+        }
+    }
+
+    /**
+     * Propertychangelistener for observer suscription.
+     * @param observer the current controller that should listen to this model
+     */
+    public void addPropertyChangeListener(PropertyChangeListener observer) {
+        support.addPropertyChangeListener(observer);
+    }
+
+    /**
+     * removes the subscription of the observer.
+     * @param observer the current controller is stopping listening to this model.
+     */
+    public void removePropertyChangeListener(PropertyChangeListener observer) {
+        support.removePropertyChangeListener(observer);
     }
 
     /**
      *
      */
-    public void setTrack() {
-        MP3File oldTrack = playlist.getPlaylist().getFirst();
-        playlist.nextTrack();
-        MP3File newTrack = playlist.getPlaylist().getFirst();
-        support.firePropertyChange("track", oldTrack, newTrack);
+    public void assignFirstPieceToTrack() {
+        this.track = playlist.getPlaylist().getFirst();
     }
-
-    
-    public void addPropertyChangeListener(PropertyChangeListener observer) {
-        support.addPropertyChangeListener(observer);
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener observer) {
-        support.removePropertyChangeListener(observer);
-    }
-
 }
